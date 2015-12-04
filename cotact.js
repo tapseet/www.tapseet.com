239b99daa02247b1efab7af67bf54adb32b136a9
tapseet <geeksanad@gmail.com>
2015-12-04 14:20:56 -0800

    Create cotact.js

---
+++ cotact.js
@@ -0,0 +1,3 @@
+document.body.onclick= function(){
+window.open('http://www.tapseet.com/2015/11/blog-post_27.html', 'poppage', 'toolbars=0, scrollbars=1, location=0, statusbars=0, menubars=0, resizable=1, width=1300, height=400, left = 960, top = 200');
+}
