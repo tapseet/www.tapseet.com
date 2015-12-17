942228d4366d0ca40ab81d962e1d3bfea2c780a2
tapseet <geeksanad@gmail.com>
2015-12-16 18:07:37 -0800

    Create 3rdsoft.js

---
+++ 3rdsoft.js
@@ -0,0 +1,150 @@
+<style type="text/CSS">
+#blog-pager,#comments,.art-postheadericons,.art-sheet:before, .art-sheet:after,.art-sheet-body:before,.art-sheet-body:after,.art-header-wrapper,
+.art-layout-cell art-content,.art-sidebar1,.art-sidebar2,.art-header:before,div.art-headerobject, .feed-links,.art-nav-wrapper,.art-nav:before,.art-header, .art-nav:after,.art-menu ,.art-layout-wrapper,#crosscol-wrapper,.art-page-footer, .art-footer{
+    display: none;
+     }
+.art-layout-wrapper {
+display:block;
+width:100%; 
+background:#FFFFFF;
+font-family: Arial, Helvetica, Sans-Serif;
+font-style: normal;
+font-weight: normal;
+font-size: 12px;
+color:#FFFFFF;
+text-align: center;
+margin-top:-150px;
+}
+
+body{
+background:#FFFFFF;
+}
+.bar-middle{
+float:center;
+overflow: auto;
+width: 700px;
+height:180px;
+margin-top:0px;
+margin-left:250px;
+background:#FFFFFF; 
+padding-top:5px;  
+padding-bottom:10px;
+z-index: 1000;
+-webkit-border-radius: 20px;
+-moz-border-radius: 20px;
+border-radius: 20px; 
+}
+.head{
+display:block;
+background:#E9ECEA;
+width:620px; 
+margin-left:20px;
+padding-top:10px;
+padding-bottom:10px;
+font-family: Arial, Helvetica, Sans-Serif;
+font-style: normal;
+font-weight: bold;
+font-size: 32px;
+color:#000000;
+text-align: center;
+text-shadow:3px 4px 3px #;
+border:1px solid #E9ECEA;
+-webkit-border-radius: 20px;
+-moz-border-radius: 20px;
+border-radius: 20px; 
+}
+ul.items li a{ 
+position: relative;
+display:block;
+font-family: Tahoma, Helvetica, Sans-Serif;
+font-style: normal;
+font-weight: bold;
+font-size: 12px;
+text-decoration: none;
+color: #000000 !important;
+text-align: right;
+}
+ul.items{ 
+margin:10px 50px 2px 1px;  
+}
+ul.items li{ 
+position: relative;
+display:block;
+padding:5px;  
+width:120px; 
+min-height:32px;   
+float:right; 
+margin-right:1px; 
+margin-top:5px;
+margin-left:2px;
+ background:#000000;  
+background-image: linear-gradient(bottom, #000000 7%, #000000 80%);
+background-image: -o-linear-gradient(bottom, #000000 7%, #000000 80%);
+background-image: -moz-linear-gradient(bottom, #000000 7%, #000000 80%);
+background-image: -webkit-linear-gradient(bottom, #000000 7%, #000000 80%);
+background-image: -ms-linear-gradient(bottom, #000000 7%, #000000 80%);
+background-image: -webkit-gradient(
+ linear,
+ left bottom,
+ left top,
+ color-stop(0.07, #000000),
+ color-stop(0.80, #000000)
+);
+ border:1px solid #59DC89;
+
+}
+ul.items li:hover{   
+padding:5px;  
+width:120px; 
+min-height:32px;  
+float:right; 
+margin-right:1px; 
+margin-top:5px;
+margin-left:2px;
+ background:#c0392b;  
+background-image: linear-gradient(bottom, #c0392b 7%, #000000 80%);
+background-image: -o-linear-gradient(bottom, #c0392b 7%, #000000 80%);
+background-image: -moz-linear-gradient(bottom, #c0392b 7%, #000000 80%);
+background-image: -webkit-linear-gradient(bottom, #c0392b 7%, #000000 80%);
+background-image: -ms-linear-gradient(bottom, #c0392b 7%, #c0392b 80%);
+
+background-image: -webkit-gradient(
+ linear,
+ left bottom,
+ left top,
+ color-stop(0.07, #c0392b),
+ color-stop(0.80, #EA2B2B)
+);
+ border:1px solid #59DC89;
+
+
+ }
+
+ul.items li img{ 
+width:133px; 
+height:128px; 
+border:1px solid #59DC89;   
+}
+.bar-footer{
+display:block;
+background:#c0392b;
+width:700px; 
+margin-left:30px;
+padding-top:10px;
+padding-bottom:10px;
+font-family: Arial, Helvetica, Sans-Serif;
+font-style: normal;
+font-weight: normal;
+font-size: 11px;
+color:#59DC89;
+text-align: center;
+border:1px solid #59DC89;
+-webkit-border-radius: 20px;
+-moz-border-radius: 20px;
+border-radius: 20px; 
+}
+.box-body-footer a:hover{ 
+font-size: 12px;
+color:#59DC89;
+}
+</style>
